9a87ade43ebae8c573c3b69b7b6b2a58ca0d49a9
Adam <a.bengtsson1988@gmail.com>
2021-12-07 10:01:47 +0100

    Added Change 2 to printout

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello World! Change 1");
+        System.out.println("Hello World! Change 2");
 
     }
 }
